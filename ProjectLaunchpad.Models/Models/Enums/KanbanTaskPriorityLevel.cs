a8b672ef4ac779ad05f46938c6aac1a8ec1f3b8c
Matiullah-Yousfani <139193387+Matiullah-Yousfani@users.noreply.github.com>
2025-07-02 11:40:43 +0500

    ok

---
+++ ProjectLaunchpad.Models/Models/Enums/KanbanTaskPriorityLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLaunchpad.Models.Models.Enums
+{
+    public enum KanbanTaskPriorityLevel
+    {
+        Low,
+        Medium,
+        High,
+        Urgent
+    }
+}
