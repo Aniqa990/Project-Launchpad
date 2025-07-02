3a43f74e18fa16e9a4622f91873d3956b6980b9b
Matiullah-Yousfani <usfanimutiullah@gmail.com>
2025-07-02 12:19:37 +0500

    first commit

---
+++ ProjectLaunchpad.Models/Models/Enums/KanbanTaskStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLaunchpad.Models.Models.Enums
+{
+    public enum KanbanTaskStatus
+    {
+        ToDo,
+        InProgress,
+        Done
+    }
+
+    
+}
---
+++ ProjectLaunchpad/Functions/ProtectedFunction.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using ProjectLaunchpad.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLaunchpad.Functions
+{
+    public class ProtectedFunction
+    {
+        private readonly TokenAuthorization _auth;
+
+        public ProtectedFunction(TokenAuthorization auth)
+        {
+            _auth = auth;
+        }
+
+        [Function("GetClientDashboard")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "client/dashboard")] HttpRequestData req)
+        {
+            (bool isAuthorized, ClaimsPrincipal? user, HttpResponseData? unauthorizedResponse) = await _auth.AuthorizeAsync(req, "Client");
+
+
+            if (!isAuthorized)
+                return unauthorizedResponse!;
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            await response.WriteStringAsync($"Welcome, Client {user!.Identity!.Name}!");
+            return response;
+        }
+    }
+}
