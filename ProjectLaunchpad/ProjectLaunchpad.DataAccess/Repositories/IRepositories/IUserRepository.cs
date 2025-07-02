95a8749950ad17b445a9a42af357f8e0f4f3c1f7
Matiullah-Yousfani <139193387+Matiullah-Yousfani@users.noreply.github.com>
2025-07-02 11:54:00 +0500

    made some changes

---
+++ ProjectLaunchpad/ProjectLaunchpad.DataAccess/Repositories/IRepositories/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using ProjectLaunchpad.Models;
+using ProjectLaunchpad.Models.Models;
+using ProjectLaunchpad.Models.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLaunchpad.DataAccess.Repositories.IRepositories
+{
+    public interface IUserRepository
+    {
+        // for login and Signup
+        Task<User> GetUserByEmailAsync(string email);
+        Task<bool> UserExistsAsync(string email);
+        Task AddUserAsync(User user);
+
+        // 
+    }
+}
---
+++ ProjectLaunchpad/ProjectLaunchpad.Models/Models/ProjectAssignment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLaunchpad.Models.Models
+{
+    public class ProjectAssignment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public Project Project { get; set; }
+
+        [Required]
+        public int FreelancerId { get; set; } // freelancer
+
+        [ForeignKey("FreelancerId")]
+        public User Freelancer { get; set; }
+    }
+
+}
