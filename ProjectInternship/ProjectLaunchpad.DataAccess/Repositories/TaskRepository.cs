ee2ee5e7beb0aa86b7ac3f9c7677d0d33d917bfa
Aniqa Azhar <aniqaazhar99@gmail.com>
2025-08-01 17:43:52 +0500

    updating project update function

--- ProjectInternship/ProjectLaunchpad.DataAccess/Repositories/TaskRepository.cs
+++ ProjectInternship/ProjectLaunchpad.DataAccess/Repositories/TaskRepository.cs
@@ -112,13 +112,6 @@ namespace ProjectLaunchpad.DataAccess.Repositories
             return taskDtos;
         }
 
-        public async Task<TaskItem?> FindTaskByTitleAndProjectAsync(string title, int projectId)
-        {
-            return await _db.taskItems
-                .FirstOrDefaultAsync(t => t.Title == title && t.projectId == projectId);
-        }
-
-
         public async Task<TaskItem?> GetByIdAsync(int id)
         {
             return await _db.taskItems.Include(t => t.Subtasks).FirstOrDefaultAsync(t => t.Id == id);
